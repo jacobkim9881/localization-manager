8da6bc9e6b6e3fcb3723af78d001f42b8420f987
jacobkim9881 <jacobkim9881@gmail.com>
2022-07-16 22:51:09 +0900

    bug: result object is not same original obj

--- index.js
+++ index.js
@@ -1,114 +1,10 @@
 const translate = require('translate-google')
 const writeFile = require('write-file')
+const langsExplain = require('./lang_code')
+const langs = Object.keys(langsExplain);
+const localObj = require('./local_obj')
 
-const langsExplain = {
-  af: 'Afrikaans',
-  sq: 'Albanian',
-  ar: 'Arabic',
-  hy: 'Armenian',
-  az: 'Azerbaijani',
-  eu: 'Basque',
-  be: 'Belarusian',
-  bn: 'Bengali',
-  bs: 'Bosnian',
-  bg: 'Bulgarian',
-  ca: 'Catalan',
-  ceb: 'Cebuano',
-  ny: 'Chichewa',
-  'zh-cn': 'Chinese Simplified',
-  'zh-tw': 'Chinese Traditional',
-  co: 'Corsican',
-  hr: 'Croatian',
-  cs: 'Czech',
-  da: 'Danish',
-  nl: 'Dutch',
-  en: 'English',
-  eo: 'Esperanto',
-  et: 'Estonian',
-  tl: 'Filipino',
-  fi: 'Finnish',
-  fr: 'French',
-  fy: 'Frisian',
-  gl: 'Galician',
-  ka: 'Georgian',
-  de: 'German',
-  el: 'Greek',
-  gu: 'Gujarati',
-  ht: 'Haitian Creole',
-  ha: 'Hausa',
-  haw: 'Hawaiian',
-  iw: 'Hebrew',
-  hi: 'Hindi',
-  hmn: 'Hmong',
-  hu: 'Hungarian',
-  is: 'Icelandic',
-  ig: 'Igbo',
-  id: 'Indonesian',
-  ga: 'Irish',
-  it: 'Italian',
-  ja: 'Japanese',
-  jw: 'Javanese',
-  kn: 'Kannada',
-  kk: 'Kazakh',
-  km: 'Khmer',
-  ko: 'Korean',
-  ku: 'Kurdish (Kurmanji)',
-  ky: 'Kyrgyz',
-  lo: 'Lao',
-  la: 'Latin',
-  lv: 'Latvian',
-  lt: 'Lithuanian',
-  lb: 'Luxembourgish',
-  mk: 'Macedonian',
-  mg: 'Malagasy',
-  ms: 'Malay',
-  ml: 'Malayalam',
-  mt: 'Maltese',
-  mi: 'Maori',
-  mr: 'Marathi',
-  mn: 'Mongolian',
-  my: 'Myanmar (Burmese)',
-  ne: 'Nepali',
-  no: 'Norwegian',
-  ps: 'Pashto',
-  fa: 'Persian',
-  pl: 'Polish',
-  pt: 'Portuguese',
-  ma: 'Punjabi',
-  ro: 'Romanian',
-  ru: 'Russian',
-  sm: 'Samoan',
-  gd: 'Scots Gaelic',
-  sr: 'Serbian',
-  st: 'Sesotho',
-  sn: 'Shona',
-  sd: 'Sindhi',
-  si: 'Sinhala',
-  sk: 'Slovak',
-  sl: 'Slovenian',
-  so: 'Somali',
-  es: 'Spanish',
-  su: 'Sudanese',
-  sw: 'Swahili',
-  sv: 'Swedish',
-  tg: 'Tajik',
-  ta: 'Tamil',
-  te: 'Telugu',
-  th: 'Thai',
-  tr: 'Turkish',
-  uk: 'Ukrainian',
-  ur: 'Urdu',
-  uz: 'Uzbek',
-  vi: 'Vietnamese',
-  cy: 'Welsh',
-  xh: 'Xhosa',
-  yi: 'Yiddish',
-  yo: 'Yoruba',
-  zu: 'Zulu'
-}
-, langs = Object.keys(langsExplain);
-
-
+console.log(langs)
 ///home/kim/tailing-mouse-footprint/_locales/en
 /*
 writeFile('foo/bar/baz/qux.txt', 'some contents', function (err) {
@@ -116,6 +12,7 @@ writeFile('foo/bar/baz/qux.txt', 'some contents', function (err) {
   console.log('file is written')
 })
 */
+
 let aPath = `/home/kim/tailing-mouse-footprint/_locales/`
 
 , appName = "Cursor Tails"
@@ -161,8 +58,51 @@ async function putLocalInMessage(lang, obj, cont1, cont2) {
  })	
 }
 
+async function localizeObj(content, lang, key) {
+return await translate(content, {to: lang, except: []}).then(res => {
+	console.log(res)
+	key = res
+return res
+
+})
+
+return
+}
+
+async function findString(newObj, lang, localObj) {
+Object.entries(localObj).forEach(async ([key, value], idx, arr) => {
+	//for (const [key, value, idx] of Object.entries(localObj)) {
+ newObj[key] = {}
+//	console.log('idx: ', idx)
+ if (typeof value === 'string') {
+  newObj[key] = await localizeObj(value, lang, newObj[key])
+ } else if (typeof value === 'object') {
+   findString(newObj, lang, value)
+ } else {
+   newObj[key] = value
+ }
+//}
+	//
+if (idx === (arr.length - 1)) {
+console.log('localized fin : ', newObj)
+}
+		
+})
+	return
+}
+
+
+for (let i = 0; i < 1; i++) {
+let newObj = {}
+	findString(newObj, langs[i], localObj)
+	console.log(newObj)
+	
+setTimeout(() => console.log(newObj), 5000)
+}
+/*
 for (let i = 0; i < 1; i++) {
  let localName = appName;
  nameWithLocalize(localName + ' - ', appName1, langs[i])	
   .then((res) => putLocalInMessage(langs[i], messages, appDesc, res));
 }
+*/
---
+++ local_obj.js
@@ -0,0 +1,9 @@
+const localObj = {
+    "H1": {
+        "0": "Nortain VPN"
+    }
+, "tt" : 1
+	, "s" : "apple"
+} 
+
+module.exports = localObj
