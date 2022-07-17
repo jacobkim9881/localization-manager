e3cdab19dd8c84070cff7cf407a3bd83611d2a4c
jacobkim9881 <jacobkim9881@gmail.com>
2022-07-17 16:15:24 +0900

    test: recursive localize already in the module

--- local_obj.js
+++ local_obj.js
@@ -2,7 +2,7 @@ const localObj = {
     "H1": {
         "0": "apple",
         "H1": {
-          "0": "apple"
+          "0": "<img class=\"icon\" src=\"\" alt=\"github social icon\" data-inject-svg=\"\">\n                  <span>Purchase NortainVPN</span>"
     }
 
     }
