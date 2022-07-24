60dff9975b6bfb5c78ee972540f82512fa9a592c
jacobkim9881 <jacobkim9881@gmail.com>
2022-07-24 22:48:26 +0900

    test: lexical scope

---
+++ test.js
@@ -0,0 +1,33 @@
+//
+let arr = [1, 2]
+, txt = 'txt'
+
+arr.forEach((val) => {
+txt = txt + 1
+})
+
+//expecting : txt11
+console.log(txt)
+
+function test(arr, txt) {
+arr.forEach((val) => {
+txt = txt + 1
+})
+return
+}
+
+test(arr, txt)
+//expecting : txt11
+console.log(txt)
+
+function test2(txt) {
+txt = txt + 2
+
+//expecting : txt112
+console.log(txt)
+return
+}
+
+test2(txt)
+//expecting : txt11
+console.log(txt)
