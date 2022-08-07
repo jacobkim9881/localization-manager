bb281151202711d0784e3bdbf16a9fd9ac8938fc
jacobkim9881 <jacobkim9881@gmail.com>
2022-08-08 02:16:58 +0900

    test: addStr not return value for testing on mocha

---
+++ utils-test.js
@@ -0,0 +1,11 @@
+const {addStr} = require('./utils')
+var assert = require('assert');
+
+const targetObj = {"key" : "value", "key2" : {"key3": "value2"}}
+, localStr = []
+
+describe('addStr', function () {
+it('should return', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+})
