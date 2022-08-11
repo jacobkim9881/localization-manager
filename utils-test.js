30c88447eeca2ba3b5639adfa9fcf11c1f735842
jacobkim9881 <jacobkim9881@gmail.com>
2022-08-12 08:15:18 +0900

    test: made first test

---
+++ utils-test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+const {writeJson, localizeObj} = require ('./utils')
+
+function testWriteJson() {
+writeJson('1.js', '1')
+
+	return
+}
+
+async function testLocalizeObj() {
+  try {
+  let value = await localizeObj('apple', 'af')
+  , typeOfValue = typeof value	
+  console.log('Testing localizeObj()')	  
+  assert.equal(typeOfValue, 'string')
+  } catch(err) {
+  console.log('test localizeObj err: ', err)
+  }
+	return
+}
+
+testLocalizeObj()
