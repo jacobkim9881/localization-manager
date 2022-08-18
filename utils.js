47d59c453c8aa914a2f7654325ead8a6a148c5bf
jacobkim9881 <jacobkim9881@gmail.com>
2022-08-19 03:30:59 +0900

    fix: translate function not work

--- utils.js
+++ utils.js
@@ -19,7 +19,7 @@ module.exports = {
     testStr(lang)	  
     //console.log(content)
     //should split content if content length is too many to execute translate
-    return content
+    //return content
     console.log('Target language: ', lang)
     return await translate(content, {to: lang, except: except}).then(res => {
     testStr(res)	  
